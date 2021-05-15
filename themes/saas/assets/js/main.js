0f79626fa944be8e1ba758be32a42ee0b96026f5
gitnodemaster <nodemasters@outlook.com>
2021-05-15 14:00:22 +0100

    Change pricing page

--- themes/saas/assets/js/main.js
+++ themes/saas/assets/js/main.js
@@ -23,42 +23,42 @@ $(document).ready(function(){
   /**
    * Pricing Table
    */
-	var monthly_price_table = $('#price-tables').find('.monthly');
-	var yearly_price_table = $('#price-tables').find('.yearly');
-	var forever_price_table = $('#price-tables').find('.forever');
+	// var monthly_price_table = $('#price-tables').find('.monthly');
+	// var yearly_price_table = $('#price-tables').find('.yearly');
+	// var forever_price_table = $('#price-tables').find('.forever');
 
-	$('.switch-toggles').find('.monthly').addClass('active');
-	$('#price-tables').find('.monthly').addClass('active');
+	// $('.switch-toggles').find('.monthly').addClass('active');
+	// $('#price-tables').find('.monthly').addClass('active');
 
-	$('.switch-toggles').find('.monthly').on('click', function(){
-		$(this).addClass('active');
-		$(this).closest('.switch-toggles').removeClass('active');
-		$(this).closest('.switch-toggles').removeClass('active1');
-		$(this).siblings().removeClass('active');
-		monthly_price_table.addClass('active');
-		yearly_price_table.removeClass('active');
-		forever_price_table.removeClass('active');
-	});
+	// $('.switch-toggles').find('.monthly').on('click', function(){
+	// 	$(this).addClass('active');
+	// 	$(this).closest('.switch-toggles').removeClass('active');
+	// 	$(this).closest('.switch-toggles').removeClass('active1');
+	// 	$(this).siblings().removeClass('active');
+	// 	monthly_price_table.addClass('active');
+	// 	yearly_price_table.removeClass('active');
+	// 	forever_price_table.removeClass('active');
+	// });
 
-	$('.switch-toggles').find('.yearly').on('click', function(){
-		$(this).addClass('active');
-		$(this).closest('.switch-toggles').addClass('active');
-		$(this).closest('.switch-toggles').removeClass('active1');
-		$(this).siblings().removeClass('active');
-		yearly_price_table.addClass('active');
-		monthly_price_table.removeClass('active');
-		forever_price_table.removeClass('active');
-	});
+	// $('.switch-toggles').find('.yearly').on('click', function(){
+	// 	$(this).addClass('active');
+	// 	$(this).closest('.switch-toggles').addClass('active');
+	// 	$(this).closest('.switch-toggles').removeClass('active1');
+	// 	$(this).siblings().removeClass('active');
+	// 	yearly_price_table.addClass('active');
+	// 	monthly_price_table.removeClass('active');
+	// 	forever_price_table.removeClass('active');
+	// });
 
-	$('.switch-toggles').find('.forever').on('click', function(){
-		$(this).addClass('active');
-		$(this).closest('.switch-toggles').addClass('active1');
-		$(this).closest('.switch-toggles').removeClass('active');
-		$(this).siblings().removeClass('active');
-		forever_price_table.addClass('active');
-		monthly_price_table.removeClass('active');
-		yearly_price_table.removeClass('active');
-	});
+	// $('.switch-toggles').find('.forever').on('click', function(){
+	// 	$(this).addClass('active');
+	// 	$(this).closest('.switch-toggles').addClass('active1');
+	// 	$(this).closest('.switch-toggles').removeClass('active');
+	// 	$(this).siblings().removeClass('active');
+	// 	forever_price_table.addClass('active');
+	// 	monthly_price_table.removeClass('active');
+	// 	yearly_price_table.removeClass('active');
+	// });
   /**
    * Fixed Header
    */
